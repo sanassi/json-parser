264da762e29e73c01fa043190e2729e5d2d904b7
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-11-26 08:14:24 +0100

    feat: lexer is ok :)

---
+++ json-parser.cc
@@ -0,0 +1,205 @@
+#include "json-parser.hh"
+#include <cctype>
+#include <cstdlib>
+#include <ostream>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+namespace jsonparse
+{
+    token::token(token_type type, std::string lexeme)
+        : type_(type),
+          lexeme_(lexeme)
+    {}
+
+    token::token(token_type type, double value)
+        : type_(type),
+          value_(value)
+    {}
+
+    token::token(token_type type)
+        : type_(type)
+    {}
+
+    const std::string token::get_lexeme() const { return lexeme_; }
+    const token_type token::get_type() const { return type_; }
+
+    lexer::lexer(std::string input)
+        : input(input),
+          input_stream(std::stringstream(input))
+    {}
+
+    std::ostream& operator<<(std::ostream& os, const token& token)
+    {
+        switch (token.type_)
+        {
+        case token_type::STRING:
+            os << "\"" << token.get_lexeme() << "\"";
+            break;
+        case token_type::NUMBER:
+            os << token.value_;
+            break;
+        default:
+            os << token.lexeme_;
+        }
+         return os;
+    }
+
+    char lexer::get_next_char()
+    {
+        char next;
+        input_stream.get(next);
+
+        return next;
+    }
+
+    char lexer::look_next_char()
+    {
+        return input_stream.peek();
+    }
+
+    token lexer::lex_string()
+    {
+        std::string res = "";
+
+        get_next_char();
+
+        while (input_stream.peek() != '\"')
+            res += get_next_char();
+
+        get_next_char();
+
+        return token(token_type::STRING, res);
+    }
+
+    token lexer::lex_alpha()
+    {
+        std::string res = "";
+
+        while (std::isalpha(look_next_char()))
+            res += get_next_char();
+
+        token_type type;
+
+        if (res == "null")
+            type = token_type::NONE;
+        else if (res == "false")
+            type = token_type::FALSE;
+        else if (res == "true")
+            type = token_type::TRUE;
+        else
+        {
+            // error
+        }
+
+        return token(type, res);
+    }
+
+    token lexer::lex_number()
+    {
+        char c;
+        std::string res = "";
+        if ((c = look_next_char()) == '-') 
+        {
+            res += get_next_char();
+
+            if (!std::isdigit(look_next_char()))
+            {
+                // error
+            }
+        }
+
+        if (look_next_char() == '0')
+        {
+            res += get_next_char();
+            // leading zeros are not allowed
+            if (std::isdigit(look_next_char()))
+            {
+                // error
+            }
+
+            if (look_next_char() != '.')
+                return token(token_type::NUMBER, 0);
+        }
+
+        while (std::isdigit(look_next_char()))
+            res += get_next_char();
+
+        if (look_next_char() == '.')
+        {
+            res += get_next_char();
+            while (std::isdigit(look_next_char()))
+                res += get_next_char();
+        }
+
+        if (look_next_char() == 'e' || look_next_char() == 'E')
+        {
+            res += get_next_char();
+            if (look_next_char() == '+' || look_next_char() == '-')
+                res += get_next_char();
+            while (std::isdigit(look_next_char()))
+                res += get_next_char();
+        }
+
+        return token(token_type::NUMBER, atof(res.c_str()));
+    }
+
+    token lexer::next_token()
+    {
+        while (std::isspace(look_next_char()))
+            get_next_char();
+
+        char c = input_stream.peek();
+        switch (c)
+        {
+        case '{':
+            get_next_char();
+            return token(token_type::LBRACE, "{");
+        case '}':
+            get_next_char();
+            return token(token_type::RBRACE, "}");
+        case '[':
+            get_next_char();
+            return token(token_type::LBRACKET, "[");
+        case ']':
+            get_next_char();
+            return token(token_type::RBRACKET, "]");
+        case ',':
+            get_next_char();
+            return token(token_type::COMMA, ",");
+        case ':':
+            get_next_char();
+            return token(token_type::COLON, ":");
+        case '\"':
+            return lex_string();
+        case '-':
+            return lex_number();
+        default:
+            if (std::isdigit(c))
+                return lex_number();
+
+            if (std::isalpha(c))
+                return lex_alpha();
+            break;
+        }
+
+        get_next_char();
+
+        return token(token_type::END, "");
+    }
+}
+
+int main(void)
+{
+    jsonparse::lexer lexer("{\n \"a\":\"hello\", \n\t \"b\": 3e+4 , \"b\":true    \n}");
+    
+    while (!lexer.input_stream.eof())
+    {
+        std::cout << lexer.next_token();
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ json-parser.hh
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <ostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+namespace jsonparse
+{
+    enum class token_type
+    {
+        STRING,
+        NUMBER,
+        TRUE,
+        FALSE,
+        NONE, // NULL
+        LBRACE,
+        RBRACE,
+        LBRACKET,
+        RBRACKET,
+        COMMA,
+        COLON,
+
+        END
+    };
+
+    class token
+    {
+    public:
+        token(token_type type, std::string lexeme);
+        token(token_type type);
+        token(token_type type, double value);
+
+        const token_type get_type() const;
+        const std::string get_lexeme() const;
+
+        friend std::ostream& operator<<(std::ostream& os, const token& token);
+
+    private:
+        token_type type_;
+        std::string lexeme_;
+        double value_;
+    };
+
+    class lexer
+    {
+    public:
+        lexer(std::string input);
+
+        token next_token();
+        token lex_string();
+        token lex_number();
+        token lex_alpha();
+
+        char get_next_char();
+        char look_next_char();
+
+        std::string input;
+        std::stringstream input_stream;
+    };
+
+    /*
+    class ast
+    {};
+
+    class json_obj : public ast
+    {
+    public:
+        json_obj();
+        ast operator [](std::string key);
+
+    private:
+        std::map<std::string, ast> values_;
+    };
+
+    class json_array : public ast
+    {
+    public:
+        json_array();
+        ast operator [](int index);
+
+    private:
+        std::vector<ast> values_;
+    };
+    */
+
+} // ! jsonparse
